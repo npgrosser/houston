7d862afd51897be4096c5cdc08cb355033e6a0a1
Nicolai Großer <nicolai.grosser@googlemail.com>
2023-07-15 00:55:57 +0200

    improved template parsing

--- src/template.rs
+++ src/template.rs
@@ -36,63 +36,37 @@ impl Display for TemplateEvaluationError {
     }
 }
 
-impl DefaultTemplateEvaluator {
-    fn evaluate_line(&self,
-                     template_line: &str,
-                     args: &[&str],
-    ) -> Result<String, TemplateEvaluationError> {
-        let mut result = template_line.to_string();
-        let mut start_index = result.find("${").map_or(-1, |i| i as isize);
-
-        while start_index != -1 {
-            let end_index = find_closing_curly_bracket(&result, start_index as usize);
-            if end_index == -1 {
-                return Err(TemplateEvaluationError::SyntaxError(
-                    "Missing closing curly bracket".to_string(),
-                ));
-            }
-
-            let cmd = &result[start_index as usize + 2..end_index as usize];
-            let command_result = self.script_runner.run_script_and_get_stdout(cmd, args).map_err(|e| {
-                TemplateEvaluationError::IoError(e)
-            })?.trim().to_string();
-
-
-            // todo: better infinite loop protection
-            //  command result should be allowed to contain "${"
-            //  just need to improve how the next start_index is found
-            if command_result.contains("${") {
-                return Err(TemplateEvaluationError::IoError(
-                    io::Error::new(io::ErrorKind::Other,
-                                   "Command result cannot contain ${".to_string())
-                ));
-            }
-
-            result = result.replace(&result[start_index as usize..end_index as usize + 1], &command_result);
-            start_index = result.find("${").map_or(-1, |i| i as isize);
-        }
-
-        Ok(result)
-    }
-}
-
 impl TemplateEvaluator for DefaultTemplateEvaluator {
     /// finds all commands in the template and runs them
-    /// commands are enclosed in as ${command}
-    fn evaluate(&self, template: &str,
-                args: &[&str],
-    ) -> Result<String, TemplateEvaluationError> {
-        let lines = template.lines();
-
+    /// commands are of the form ${command}
+    fn evaluate(&self, template: &str, args: &[&str]) -> Result<String, TemplateEvaluationError> {
         let mut result = String::new();
+        let mut remaining_text = template;
+        loop {
+            let open_index = remaining_text.find("${");
+            match open_index {
+                Some(open_index) => {
+                    let close_index = find_closing_curly_bracket(remaining_text, open_index);
+                    if close_index == -1 {
+                        return Err(TemplateEvaluationError::SyntaxError(
+                            "Missing closing curly bracket".to_string(),
+                        ));
+                    }
 
-        for line in lines {
-            let evaluated_line = self.evaluate_line(line, args)?;
-            result.push_str(&evaluated_line);
-            result.push('\n');
-        };
+                    let cmd = &remaining_text[open_index + 2..close_index as usize];
+                    let cmd_out = self.script_runner.run_script_and_get_stdout(
+                        cmd, args).map_err(TemplateEvaluationError::IoError)?;
 
-        Ok(result)
+                    result.push_str(&remaining_text[..open_index]);
+                    result.push_str(cmd_out.trim_end());
+                    remaining_text = &remaining_text[close_index as usize + 1..];
+                }
+                None => {
+                    result.push_str(remaining_text);
+                    return Ok(result);
+                }
+            }
+        }
     }
 }
 
@@ -125,6 +99,7 @@ fn find_closing_bracket(
 #[cfg(test)]
 mod tests {
     use std::collections::HashMap;
+    use crate::template::TemplateEvaluationError::{IoError, SyntaxError};
     use super::*;
 
 
@@ -160,16 +135,12 @@ mod tests {
     }
 
     impl MockScriptRunner {
-        pub fn new(input_output_map: HashMap<String, String>) -> Self {
-            MockScriptRunner { input_output_map }
-        }
-
         pub fn new_from_vec(input_output_vec: Vec<(&str, &str)>) -> Self {
             let mut map = HashMap::new();
             for (input, output) in input_output_vec {
                 map.insert(input.to_string(), output.to_string());
             }
-            MockScriptRunner::new(map)
+            MockScriptRunner { input_output_map: map }
         }
 
         pub fn new_single_command(input: &str, output: &str) -> Self {
@@ -177,41 +148,46 @@ mod tests {
             map.insert(input.to_string(), output.to_string());
             MockScriptRunner { input_output_map: map }
         }
+
+        pub fn new_no_commands() -> Self {
+            MockScriptRunner { input_output_map: HashMap::new() }
+        }
     }
 
     impl ScriptRunner for MockScriptRunner {
         fn run_script(&self, script: &str,
                       _args: &[&str],
                       _handle_stdout: Option<&mut dyn FnMut(&str)>) -> io::Result<()> {
-            assert!(self.input_output_map.get(script).is_some());
-            Ok(())
+            self.run_script_and_get_stdout(script, _args).map(|_| ())
         }
 
         fn run_script_and_get_stdout(&self, script: &str, _args: &[&str]) -> io::Result<String> {
-            let result = self.input_output_map.get(script).unwrap();
-            Ok(result.to_string())
+            return match self.input_output_map.get(script) {
+                Some(value) => Ok(value.to_string()),
+                None => Err(io::Error::new(io::ErrorKind::NotFound, "not found")),
+            };
         }
     }
 
     #[test]
-    fn test_evaluate_line() {
+    fn test_evaluate() {
         let script_runner = Box::new(MockScriptRunner::new_single_command(
             "echo world", "world"));
         let evaluator = DefaultTemplateEvaluator::new(script_runner);
 
-        let result = evaluator.evaluate_line("hello ${echo world}", &[]);
+        let result = evaluator.evaluate("hello ${echo world}", &[]);
 
         assert_eq!(result.unwrap(), "hello world");
     }
 
     #[test]
-    fn test_evaluate_line_with_multiple_commands() {
+    fn test_evaluate_with_multiple_commands() {
         let script_runner = Box::new(MockScriptRunner::new_single_command(
             "echo world", "world"));
 
         let evaluator = DefaultTemplateEvaluator::new(script_runner);
 
-        let result = evaluator.evaluate_line("hello ${echo world} ${echo world}", &[]);
+        let result = evaluator.evaluate("hello ${echo world} ${echo world}", &[]);
 
         assert_eq!(result.unwrap(), "hello world world");
     }
@@ -223,23 +199,21 @@ mod tests {
             "echo world", "world"));
         let evaluator = DefaultTemplateEvaluator::new(script_runner);
 
-        let result = evaluator.evaluate_line("hello ${echo world} ${echo world} world", &[]);
+        let result = evaluator.evaluate("hello ${echo world} ${echo world} world", &[]);
         assert_eq!(result.unwrap(), "hello world world world");
     }
 
     #[test]
-    fn test_evaluate_line_with_inner_brackets() {
+    fn test_evaluate_line_with_nested_commands() {
         let script_runner = Box::new(MockScriptRunner::new_single_command(
-            "echo ${echo world}", "echo world") // todo: update when inner ${} is supported
+            "echo '${echo world}'", "${echo world}")
         );
 
         let evaluator = DefaultTemplateEvaluator::new(script_runner);
 
-        let result = evaluator.evaluate_line("hello ${echo ${echo world}}", &[]);
-        println!("{:?}", result);
-        assert_eq!(result.unwrap(), "hello echo world");
+        let result = evaluator.evaluate("hello ${echo '${echo world}'}", &[]);
+        assert_eq!(result.unwrap(), "hello ${echo world}");
     }
-
 
     #[test]
     fn test_multi_line_template() {
@@ -254,6 +228,27 @@ mod tests {
         let result = evaluator.evaluate(
             "hello ${echo world}\nmy name is ${get name}", &[]);
 
-        assert_eq!(result.unwrap(), "hello world\nmy name is john\n");
+        assert_eq!(result.unwrap(), "hello world\nmy name is john");
+    }
+
+    #[test]
+    fn test_syntax_error() {
+        let script_runner = Box::new(MockScriptRunner::new_single_command(
+            "echo world", "world"));
+        let evaluator = DefaultTemplateEvaluator::new(script_runner);
+
+        let result = evaluator.evaluate("hello ${echo world", &[]);
+
+        assert!(matches!(result, Err(SyntaxError(_))), "Expected SyntaxError, got {:?}", result);
+    }
+
+    #[test]
+    fn test_commando_error() {
+        let script_runner = Box::new(MockScriptRunner::new_no_commands());
+        let evaluator = DefaultTemplateEvaluator::new(script_runner);
+
+        let result = evaluator.evaluate("hello ${echo world} ${echo world} ${echo world}", &[]);
+
+        assert!(matches!(result, Err(IoError(_))), "Expected IoError, got {:?}", result);
     }
 }
